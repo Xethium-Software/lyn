13adf8cc9a46e24ec5252ceab4ef5ee14145c576
ShaderHex <barkott9@gmail.com>
2025-01-07 14:37:45 -0500

    Add Information texts

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,31 @@
 
 int main() {
     initscr();
-    printw("Hello, World!");
-    refresh();
-    getch();
+    cbreak();
+    noecho();
+    curs_set(0);
+
+    int max_y, max_x;
+    int y, x;
+    int ch;
+
+    while (1) {
+        getmaxyx(stdscr, max_y, max_x);
+        y = max_y / 2;
+        x = max_x / 2 - 8;
+
+        clear();
+        mvprintw(1, 1, "Lyn text editor.");
+
+        mvprintw(60,1, "-- Normal Mode --");
+        refresh();
+
+        ch = getch();
+        if (ch == 27) {
+            break;
+        }
+    }
+
     endwin();
     return 0;
 }
-
