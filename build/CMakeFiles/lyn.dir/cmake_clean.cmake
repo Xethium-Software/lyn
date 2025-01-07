a9554c8459fc651d6729089239dd9cb8aa675657
ShaderHex <barkott9@gmail.com>
2025-01-07 11:35:46 -0500

    Base system

---
+++ build/CMakeFiles/lyn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lyn.dir/src/main.cpp.o"
+  "CMakeFiles/lyn.dir/src/main.cpp.o.d"
+  "lyn"
+  "lyn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lyn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
